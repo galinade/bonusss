00b1ea4f521cd755f5ad999e60bdc3fbf0493b9f
Galina Demidova <galinagriz77@gmail.com>
2022-04-26 14:40:33 +0300

    new data

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         int check = 100; //рублей
-        int sum = 1100; //рублей
+        int sum = 100; //рублей
         int bonus;
         if (sum > 1000) {
             bonus = sum / 100;
